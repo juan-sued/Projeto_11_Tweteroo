8fee81011d62829eea2241f350586a630a009f79
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-06-17 10:48:03 -0300

    projeto finalizado

--- tweteroo/tweteroo/backend/src/Server.js
+++ tweteroo/tweteroo/backend/src/Server.js
@@ -7,33 +7,26 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const users = [
-  {
-    username: 'bobesponja',
-    avatar:
-      'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info'
-  }
-];
+const users = [];
 
-const tweets = [
-  {
-    username: 'bobesponja',
-    tweet: 'eu amo o hub'
-  }
-];
+const tweets = [];
 
 server.post('/sign-up', (request, response) => {
   users.push({ username: request.body.username, avatar: request.body.avatar });
   response.send(users);
+  response.send('OK');
 });
 
 server.post('/tweets', (request, response) => {
+  const avatar = users.find(user => user.username === request.body.username);
   tweets.push({
     username: request.body.username,
-    tweet: request.body.tweet
+    tweet: request.body.tweet,
+    avatar: avatar.avatar
   });
   const lastTen = tweets.filter((tweet, index) => index < 10);
   response.send(lastTen);
+  response.send('OK');
 });
 
 server.get('/tweets', (request, response) => {
